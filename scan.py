b55acb9368b1134da2c464fe89dd9cdb9fd7f582
abenku <abenku97@gmail.com>
2024-04-08 15:46:43 +0900

    fix: 변수 명 오탈자 수정

--- scan.py
+++ scan.py
@@ -2,34 +2,34 @@ from scapy.all import *
 
 import db
 
-ports = db.get_well_known_port()
-
 def well_known_port_scan(target):
     ports_info = db.get_all_list()
-
     scan_result = []
-    for port_info in ports_info:
+
+    for port_info in ports_info[:19]:
         port = port_info['port_number']
         try:
             ip = IP(dst=target)
             tcp = TCP(dport=port, flags="S")
-            packet = ip/tcp
+            packet = ip / tcp
             response = sr1(packet, timeout=2, verbose=0)
+
             if response is None:
                 status = 'none'
-            elif response.haslayer(TCP): # TCP레이어 존재 여부 확인
-                if response.getlayer(TCP).flags == 0x12: # 해당 레이어의 flag 확인
+            elif response.haslayer(TCP):
+                if response.getlayer(TCP).flags == 0x12:
                     status = 'open'
                 elif response.getlayer(TCP).flags == 0x14:
                     status = 'closed'
         except Exception as e:
             print(f"{target}:{port} - {e}")
-            
+            status = 'error'
+
         scan_result.append({
             "port": port,
             "status": status,
-            "service": port_info['service_namea'],
+            "service": port_info['service_name'],
             "description": port_info['port_description']
         })
+
     return scan_result
-        
